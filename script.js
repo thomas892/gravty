9122770aaeec994234a12d1dc413282df4dcfbe6
Batka Tamás <Batka Tams>
2016-06-23 13:31:20 +0200

    ClickAddWorks

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ requirejs(['physicsjs', 'jquery'], function(Physics) {
 		var viewportWidth = document.getElementById('viewport').scrollWidth;
 		var viewportHeight = document.getElementById('viewport').scrollHeight;
 		var bounds = Physics.aabb(0, 0, viewportWidth, viewportHeight);
+		
 		document.getElementById('viewport').addEventListener('click', addCircle);
 		
 		var renderer = Physics.renderer('canvas', {
@@ -28,35 +29,34 @@ requirejs(['physicsjs', 'jquery'], function(Physics) {
 		});
 		world.add(renderer);
 		
-		var circle = Physics.body('circle', {
+		/*var circle = Physics.body('circle', {
 			x: 250,
 			y: 3,
 			radius: 3,
 			vy: -0.01
-		});
-		world.add([circle, 
+		});*/
+		world.add([ 
 		Physics.behavior('constant-acceleration'),
-		Physics.behavior('edge-collision-detection', {aabb: bounds, restitution: 0.8}), 
-		Physics.behavior('body-impulse-response')
+		Physics.behavior('edge-collision-detection', {aabb: bounds, restitution: 1}), 
+		Physics.behavior('body-impulse-response'),
+		Physics.behavior('body-collision-detection'),
+		Physics.behavior('sweep-prune')
 		]);
 		
-		/*
+		
 		function addCircle(e) {
-			var xPos = e.clientX;
-			var yPos = e.clientY;
+			var xPos = e.pageX - $('#viewport').offset().left;
+			var yPos = e.pageY - $('#viewport').offset().top;
 			var circle = Physics.body('circle', {
 			x: xPos,
 			y: yPos,
-			radius: 3,
+			radius: 10,
 			vy: -0.01
 			});
 			world.add(circle);
 		}
-		*/
 		
-
-		world.add( Physics.behavior('body-collision-detection') );
-		world.add( Physics.behavior('sweep-prune') );
+		
 		world.render();
 		Physics.util.ticker.on(function (time, dt) {
 			world.step(time);

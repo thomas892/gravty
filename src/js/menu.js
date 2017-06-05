19095fd4d3ba1a54651532f114f271bc9c95d6c8
Thomas Batka <batkatom@gmail.com>
2017-06-05 13:31:41 +0200

    binding buttons

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,21 +1,27 @@
+function bindToMultipleButtons(htmlCollection, fn) {
+    for(let i = 0; i < htmlCollection.length; i++) {
+        htmlCollection[i].addEventListener('click', fn);
+    }
+}
+
 export default function bindMenuEvents(world) {
 
     let small = document.getElementById('smallButton');
     let medium = document.getElementById('mediumButton');
     let large = document.getElementById('largeButton');
     let huge = document.getElementById('hugeButton');
-    let gravityWell = document.getElementById('gravityWellButton');
-    let reset = document.getElementById('resetButton');
-    let pause = document.getElementById('pauseButton');
+    let gravityWell = document.getElementsByClassName('gravityWellButton');
+    let reset = document.getElementsByClassName('resetButton');
+    let pause = document.getElementsByClassName('pauseButton');
 
 
-    gravityWell.addEventListener('click', () => { world.settings.gravityWellOn() });
+    bindToMultipleButtons(gravityWell, () => { world.settings.gravityWellOn()});
+    bindToMultipleButtons(reset, () => { world.remove(world.getBodies()) });
+    bindToMultipleButtons(pause, () => { world.isPaused() ? world.unpause() : world.pause() });
     small.addEventListener('click', () => { world.settings.changeRadius(2); });
     medium.addEventListener('click', () => { world.settings.changeRadius(4);  });
     large.addEventListener('click', () => { world.settings.changeRadius(6);  });
     huge.addEventListener('click', () => { world.settings.changeRadius(8); });
-    reset.addEventListener('click', () => { world.remove(world.getBodies()) });
-    pause.addEventListener('click', () => { world.isPaused() ? world.unpause() : world.pause() });
 
 
 
